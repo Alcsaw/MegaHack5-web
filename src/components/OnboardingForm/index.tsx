43f7b14cf0303fe977cc0e0be267bd7aa4553c63
Augusto Schnorr <alcsaw@hotmail.com>
2020-11-15 15:48:58 -0300

    Styles questions in onboarding form

--- src/components/OnboardingForm/index.tsx
+++ src/components/OnboardingForm/index.tsx
@@ -1,20 +1,33 @@
 import React from 'react';
 
-import { Container, Form, Question } from './styles';
+import { Container, Form, Label, Question, Select, Button } from './styles';
 
 const OnboardingForm: React.FC = () => {
   return (
     <Container>
       <Form>
 
-        <label htmlFor="favorite_color">
-          <Question>Qual a sua cor favorita?</Question>
-        </label>
-        <select name="favorito_color" id="favorito_color">
-          <option value="blue">Azul</option>
-          <option value="red">Vermelho</option>
-          <option value="yellow">Amarelo</option>
-        </select>
+        <Question>
+          <Label htmlFor="favorite_color">Qual a sua cor favorita?</Label>
+          <Select name="favorito_color" id="favorito_color">
+            <option value="blue">Azul</option>
+            <option value="red">Vermelho</option>
+            <option value="yellow">Amarelo</option>
+          </Select>
+        </Question>
+
+        <Question>
+          <Label htmlFor="body_size">Qual é o formato do seu corpo?</Label>
+          <Select name="favorito_color" id="favorito_color">
+            <option value="thin">Magro</option>
+            <option value="meso">Mediano</option>
+            <option value="fat">Sobrepeso</option>
+            <option value="muscle">Musculoso</option>
+          </Select>
+        </Question>
+
+        <Button type="submit">Pronto</Button>
+
       </Form>
     </Container>
   );
--- src/components/OnboardingForm/styles.ts
+++ src/components/OnboardingForm/styles.ts
@@ -3,20 +3,60 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
 
+  margin-top: 2rem;
   margin-bottom: 4rem;
 `;
 
 export const Form = styled.form`
   font-family: 'Lato',sans-serif;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
-export const Question = styled.span`
+export const Question = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 4rem;
+`;
+
+export const Label = styled.label`
   text-align: center;
-  margin-top: 50px;
   margin-right: 20px;
 
   font-family: 'Lato', 'sans-serif';
   font-size: 32px;
   font-weight: 700;
   text-transform: uppercase;
+`;
+
+export const Select = styled.select`
+  border: none;
+  border-bottom: 2px solid var(--modal-black);
+  background: var(--white);
+  cursor: pointer;
+  padding: 15px 15px 10px 15px;
+
+  -webkit-appearance: none;
+   -moz-appearance: none;
+   appearance: none;
+`;
+
+export const Button = styled.button`
+  border: 0;
+  background: var(--modal-black);
+  color: var(--white);
+  text-transform: uppercase;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  width: 152px;
+
+  padding: 16px 24px;
+  font-size: 14px;
+  justify-content: center;
 `;
